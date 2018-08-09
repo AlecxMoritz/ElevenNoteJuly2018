d384dc108514faf4c6a09bf164594f4bfc5430b8
Alecx Moritz <hanelore@live.com>
2018-08-08 20:25:37 -0400

    12: Star Setup

--- Elevennote.Web.MVC/Global.asax.cs
+++ Elevennote.Web.MVC/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Elevennote.Web.MVC.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,7 @@ namespace Elevennote.Web.MVC
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            WebApiConfig.Register();
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
